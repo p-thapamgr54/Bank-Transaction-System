f0d9dc2d9037a700e12fb36b995c5189a26d6bbc
Pankaj Thapa <p.thapamgr54@gmail.com>
2026-07-22 00:17:43 +0545

    Database Configuration Setup

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+import "dotenv/config";
 
-app.listen(3000, (req, res) => {
-  console.log(`Your server is running on port 3000`);
+const PORT = process.env.PORT || 3000; // Define the port to listen on, defaulting to 3000 if not specified in environment variables
+
+// Connect to the MongoDB database
+connectDB();
+app.listen(PORT, (req, res) => {
+  console.log(`Your server is running on http://localhost:${PORT}`); // Log a message indicating that the server is running and on which port
 });
